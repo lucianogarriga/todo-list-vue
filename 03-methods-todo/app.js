2b4c76fd9a5c72e12119a34561f324ced4cd7e32
Luciano Garriga <lucianomgarriga@gmail.com>
2023-11-25 20:04:32 -0300

    Add localStorage in Methods-TODO

--- 03-methods-todo/app.js
+++ 03-methods-todo/app.js
@@ -7,20 +7,33 @@ const app = new Vue({
         newTasks: ''
     },
     methods:{
+        addLocalStorage: function(){ 
+            localStorage.setItem('gym-vue', JSON.stringify(this.tasks));
+        },
         addTasks: function(){
             //this = p/ referirnos a 'data'
             this.tasks.push({
                 name: this.newTasks,
                 state: false
-            }); 
-            console.log(this.tasks);
-            this.newTasks = ''
+            });  
+            this.newTasks = '';
+            this.addLocalStorage();
         },
         changeState: function(index){
             this.tasks[index].state = !this.state;
+            this.addLocalStorage();
         },
         removeTask: function(index){
-            this.tasks.splice(index,1);
+            this.tasks.splice(index,1);            
+            this.addLocalStorage();
+        }
+    },
+    created: function(){
+        let dataLS = JSON.parse(localStorage.getItem('gym-vue')); 
+        if(dataLS === null){
+            this.tasks = [];
+        } else {
+            this.tasks = dataLS;
         }
     }
 })
