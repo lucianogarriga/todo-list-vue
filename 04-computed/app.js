145d0b07dbd2dc3b475e747428746e4d5bdb7806
Luciano Garriga <lucianomgarriga@gmail.com>
2023-11-27 20:57:44 -0300

    Add logic & styles in Input to reverseMessages / Counter w/ progressBar

--- 04-computed/app.js
+++ 04-computed/app.js
@@ -4,9 +4,27 @@ const app = new Vue({
         mensaje: "",
         contador: 0
     },
+    methods:{
+        sumar(){
+            this.contador<100 ? this.contador += 5 : this.contador;
+            console.log(this.contador);
+        },
+        restar(){
+            this.contador>0 
+                ? this.contador -= 5 
+                : alert("El contador no puede ser negativo");
+        }
+    },
     computed:{
         invertido(){
             return this.mensaje.split('').reverse().join('');
+        },
+        color(){
+            return {
+                'bg-success': this.contador <= 100,
+                'bg-warning': this.contador < 66 && this.contador > 21,
+                'bg-danger': this.contador < 20 && this.contador > 0
+            }
         }
     } 
 })
